5a675a731bdc92d19daabddf24615a90ed9a20a1
collinewait <collinewait17@gmail.com>
2019-05-03 16:58:49 +0300

    create a flexbox component and add it to the routes

--- src/app/demo/demo-routing.module.ts
+++ src/app/demo/demo-routing.module.ts
@@ -1,9 +1,11 @@
 import { ButtonsComponent } from './buttons/buttons.component';
+import { FlexboxComponent } from './flexbox/flexbox.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'buttons', component: ButtonsComponent },
+  { path: 'flexbox', component: FlexboxComponent },
   { path: '**', redirectTo: 'buttons'}
 ];
 
